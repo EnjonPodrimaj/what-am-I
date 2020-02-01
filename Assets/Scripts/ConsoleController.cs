b72a6add52557f5955d3c89e9d9bf79763a72db7
ilirvg <ilir_gruda@hotmail.com>
2020-02-01 01:55:19 +0100

    Ready for everyone

---
+++ Assets/Scripts/ConsoleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleController : MonoBehaviour {
+    //public static Action<bool> SendGameObject;
+    //public static Action<bool> DisablePlayerControllersAction;
+
+    [SerializeField] private GameObject _debugPanel;
+    public GameObject _downButton;
+    public GameObject _leftButton;
+
+    public void OpenDebugOptions() {
+        _debugPanel.SetActive(true);
+        //_leftButton.transform.position = _downButton.transform.position;
+        _downButton.SetActive(false);
+        
+    }
+}
---
+++ Assets/Scripts/DebugController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugController : MonoBehaviour {
+    public ConsoleController consoleController;
+    public void CodeClicked(string code) {
+        if(code == "Left") {
+            consoleController._leftButton.transform.position = consoleController._downButton.transform.position;
+            
+        }
+        
+    }
+
+}
